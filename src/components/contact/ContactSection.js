83ee7e303a6ef6a36aa7a63b971bbd6a1848fbfe
Vivat Chongsomchit <91275232+mostvivat@users.noreply.github.com>
2024-03-25 09:57:21 +0700

    Update Email in contact.js

--- src/components/contact/ContactSection.js
+++ src/components/contact/ContactSection.js
@@ -14,12 +14,12 @@ export default function ContactSection() {
         <div className="contactSection__wrapper">
           <div className="left">
             <ContactInfoItem icon={<MdLocalPhone />} text="+66971595272" />
-            <ContactInfoItem icon={<MdEmail />} text="pokemost@gmail.com" />
+            <ContactInfoItem icon={<MdEmail />} text="most.vivat@gmail.com" />
             <ContactInfoItem text="Bangkok, Thailand" />
           </div>
           <div className="right">
               <ContactSocialsItem iconPath="skills/icons8-github.svg" text="GitHub" link="https://github.com/mostvivat" />
-              <ContactSocialsItem iconPath="skills/icons8-linkedin-50.svg" text="LinkedIn" link="https://www.linkedin.com/in/vivat-chongsomchit-72108a275/"/>
+              <ContactSocialsItem iconPath="skills/icons8-linkedin-50.svg" text="LinkedIn" link="www.linkedin.com/in/vivatchongsomchit"/>
               <ContactSocialsItem iconPath="/skills/icons8-facebook.svg" text="Facebook" link="https://www.facebook.com/vivatmost" />
           </div>
         </div>
